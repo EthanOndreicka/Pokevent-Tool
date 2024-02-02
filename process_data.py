ad1dbfc5e8bbeb99bc61f955183226da221c7ff3
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2024-02-02 14:27:49 -0500

    added scraping for thumbnail img url

--- process_data.py
+++ process_data.py
@@ -27,6 +27,8 @@ def defaultEventParse():
     start_day = soup.find('span', {'id': 'event-date-start'})
     end_day = soup.find('span', {'id': 'event-date-end'})
     start_time = '10:00am'
+    event_image = soup.select('div.image img')
+
 
     if event_title:
         print("Title: " + event_title.text)
@@ -51,6 +53,13 @@ def defaultEventParse():
         print('End date not found')
     
     print(start_time)
+
+    if event_image:
+        for img_tag in event_image:
+            event_image_text = "https://leekduck.com" + img_tag['src']
+            print("Image URL: ", event_image_text)
+    else:
+        print('URL Not Found')
 
     event_details = {"title": event_title.text if event_title else '', "description": [desc.text for desc in description], "starting": start_day_text, "ending": end_day_text, 'startingTime': start_time}
     
